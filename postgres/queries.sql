ca8c79a71535c1a91fa27bc2481d140945a8532c
Vanisha Bagga <vanishabagga@Vanishas-MacBook-Pro.local>
2024-04-05 20:50:31 -0400

    edits

--- postgres/queries.sql
+++ postgres/queries.sql
@@ -4,8 +4,9 @@
 SELECT D.year, D.weekday, D.month, D.day, SUM(F.num_deaths) as total_number_of_deaths
 FROM incident_fact_table as F
 INNER JOIN date_dimension as D ON F.date_primary_key = D.date_primary_key
-WHERE D.year = 2023 AND D.month = 1
+WHERE D.month = 3
 GROUP BY D.year, D.weekday, D.month, D.day;
+
 
 /*Roll Up Query (total deaths cases rollup to region and city)*/
 
@@ -13,19 +14,22 @@ SELECT L.state AS region, L.city AS city, SUM(F.num_deaths) AS total_death_cases
 FROM incident_fact_table AS F
 INNER JOIN location_dimension AS L ON F.location_primary_key = L.location_primary_key
 GROUP BY ROLLUP(L.state, L.city)
+HAVING L.city IS NOT NULL 
 ORDER BY L.state, L.city;
+
 
 /* Slice Query (total Cases in shootings that occurred in schools) */
 
-SELECT L.location, 
+SELECT F.case_primary_key,
+       L.location, 
        SUM(F.num_injured) AS total_injured_cases, 
-       SUM(F.num_deaths) AS total_death_cases, 
-       COUNT(P.participant_primary_key) AS total_cases
+       SUM(F.num_deaths) AS total_death_cases 
 FROM incident_fact_table AS F 
 INNER JOIN location_dimension L ON F.location_primary_key = L.location_primary_key 
 INNER JOIN participant_dimension P ON F.participant_primary_key = P.participant_primary_key
 WHERE L.location = 'school'
-GROUP BY L.location;
+GROUP BY F.case_primary_key, L.location;
+
 
 /* Dice Query (total number of injured cases by gender and race) */
 
@@ -34,14 +38,20 @@ FROM incident_fact_table AS F
 INNER JOIN participant_dimension AS P ON F.participant_primary_key = P.participant_primary_key
 GROUP BY gender, race;
 
-/* Dice Query (total sales of video games by genre, platform, publisher, and year with conditions) */
 
-SELECT V.genre, V.platform, V.publisher, V.year, SUM(G.global_sales) AS total_sales
-FROM game_sales_fact_table AS G
-INNER JOIN video_game_dimension AS V ON G.video_game_primary_key = V.video_game_primary_key
-WHERE V.genre IN ('Shooter', 'Action') 
-AND V.platform IN ('PS3', 'X360')
-GROUP BY V.genre, V.platform, V.publisher, V.year;
+/* Dice Query (total number of video games that meet genre and year conditions with also considering the correlation between year of shooting and year of game release) */
+
+SELECT gd.year,
+       gd.genre,
+       gd.platform,
+       COUNT(*) AS game_count
+FROM game_dimension gd
+INNER JOIN game_sales_fact_table gs ON gd.game_primary_key = gs.game_primary_key
+INNER JOIN game_date_association gda ON gs.video_game_primary_key = gda.game_sales_key
+INNER JOIN date_dimension dd ON gda.date_primary_key = dd.date_primary_key
+WHERE (gd.genre = 'shooter' OR gd.genre = 'action')
+AND gd.year BETWEEN 2000 AND 2023
+GROUP BY gd.year, gd.genre, gd.platform;
 
 
 /* Combining OLAP Operations - 3 Queries*/
@@ -66,33 +76,38 @@ JOIN
 LEFT JOIN 
     participant_dimension pd ON id.participant_primary_key = pd.participant_primary_key
 WHERE 
-    ld.state IN ('California', 'Texas', 'Florida')
+    ld.state IN ('california', 'texas', 'florida')
 GROUP BY 
     ld.state, vd.year, vd.month;
 
 
-/* Annual incident data by state, including total incidents, participants, injuries, and deaths.
-   Correlates incidents with the count of action or shooter video games */
+/* Incidents, injuries, deaths, participant demographics, and video game genres 
+   grouped by state and year. Calculates the number of action and shooter games associated with
+   these incidents */
 
 SELECT 
     ld.state,
     vd.year,
-    COUNT(id.case_primary_key) AS total_incidents,
-    COUNT(DISTINCT pd.participant_primary_key) AS total_participants,
-    SUM(id.num_injured) AS total_injured,
-    SUM(id.num_deaths) AS total_deaths,
+    COUNT(ift.case_primary_key) AS total_incidents,
+    COUNT(DISTINCT ift.participant_primary_key) AS total_participants,
+    SUM(ift.num_injured) AS total_injured,
+    SUM(ift.num_deaths) AS total_deaths,
     AVG(pd.age) AS avg_age,
-    SUM(CASE WHEN vg.genre IN ('Action', 'Shooter') THEN 1 ELSE 0 END) AS action_shooter_games
+    SUM(CASE WHEN gd.genre IN ('action', 'shooter') THEN 1 ELSE 0 END) AS action_shooter_games
 FROM 
-    incident_fact_table id
+    incident_fact_table ift
 JOIN 
-    location_dimension ld ON id.location_primary_key = ld.location_primary_key
+    location_dimension ld ON ift.location_primary_key = ld.location_primary_key
 JOIN 
-    date_dimension vd ON id.date_primary_key = vd.date_primary_key
+    date_dimension vd ON ift.date_primary_key = vd.date_primary_key
 LEFT JOIN 
-    participant_dimension pd ON id.participant_primary_key = pd.participant_primary_key
+    participant_dimension pd ON ift.participant_primary_key = pd.participant_primary_key
 LEFT JOIN 
-    video_game_dimension vg ON id.year = vg.year
+    game_date_association gda ON vd.date_primary_key = gda.date_primary_key
+LEFT JOIN 
+    game_sales_fact_table gsft ON gda.game_sales_key = gsft.video_game_primary_key
+LEFT JOIN 
+    game_dimension gd ON gsft.game_primary_key = gd.game_primary_key
 GROUP BY 
     ld.state, vd.year;
 
@@ -106,10 +121,6 @@ SELECT
     COUNT(id.case_primary_key) AS total_incidents,
     COUNT(DISTINCT pd.participant_primary_key) AS total_participants,
     AVG(pd.age) AS avg_age,
-    SUM(CASE WHEN pd.gender = 'Male' THEN 1 ELSE 0 END) AS total_male_participants,
-    SUM(CASE WHEN pd.gender = 'Female' THEN 1 ELSE 0 END) AS total_female_participants,
-    SUM(CASE WHEN pd.health_flag = true THEN 1 ELSE 0 END) AS total_healthy_participants,
-    SUM(CASE WHEN pd.health_flag = false THEN 1 ELSE 0 END) AS total_unhealthy_participants,
     SUM(gs.na_sales) AS total_na_sales,
     SUM(gs.eu_sales) AS total_eu_sales,
     SUM(gs.jp_sales) AS total_jp_sales,
@@ -122,9 +133,32 @@ JOIN
 JOIN 
     date_dimension vd ON id.date_primary_key = vd.date_primary_key
 LEFT JOIN 
-    game_sales_fact_table gs ON vd.year = gs.year
+    game_date_association gda ON vd.date_primary_key = gda.date_primary_key
+LEFT JOIN 
+    game_sales_fact_table gs ON gda.game_sales_key = gs.video_game_primary_key
 GROUP BY 
-    pd.race, vd.year;
+    pd.race, vd.year
+ORDER BY
+	vd.year;
+
+/* Video game titles, genres, release years, injuries, and deaths */
+
+SELECT 
+    gd.title,
+    gd.genre,
+    gd.year,
+    ift.num_injured,
+    ift.num_deaths
+FROM 
+    game_dimension gd
+JOIN 
+    game_sales_fact_table gsft ON gd.game_primary_key = gsft.game_primary_key
+JOIN 
+    game_date_association gda ON gsft.video_game_primary_key = gda.game_sales_key
+JOIN 
+    date_dimension dd ON gda.date_primary_key = dd.date_primary_key
+LEFT JOIN 
+    incident_fact_table ift ON dd.date_primary_key = ift.date_primary_key;
 
 
 /* Iceberg Query (Top-N for Total Deaths on a Date) */
@@ -135,6 +169,7 @@ WHERE F.date_primary_key = D.date_primary_key
 GROUP BY D.date_primary_key, D.month, D.day, D.year
 ORDER BY total_deaths DESC
 LIMIT 5;
+
 
 /*Windowing Query (Total Cases partitioned by age_group, ranked by total and by acquisition group) */
 
@@ -148,7 +183,8 @@ JOIN participant_dimension p ON f.participant_primary_key = p.participant_primar
 GROUP BY
        p.age_group;
 
-/*Window Clause Query (Number of deaths in the year 2023 compared perivous years) */
+
+/*Window Clause Query (Number of deaths in the year 2023 compared previous years) */
 
 SELECT
     year,
@@ -156,16 +192,16 @@ SELECT
     MAX(total_casualties) OVER w AS previous_year_casualties
 FROM (
     SELECT
-        EXTRACT(year FROM d.date) AS year,
+        year,
         SUM(num_injured + num_deaths) AS total_casualties
     FROM
         incident_fact_table f
     JOIN
         date_dimension d ON f.date_primary_key = d.date_primary_key
     WHERE
-        EXTRACT(year FROM d.date) <= 2023
+        year <= 2023
     GROUP BY
-        EXTRACT(year FROM d.date)
+        year
 ) AS yearly_casualties
 WINDOW w AS (ORDER BY year ROWS BETWEEN 1 PRECEDING AND 1 PRECEDING);
 
